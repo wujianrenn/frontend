1ca12b5c9a8dee73ea6f046ffc3382652361931e
wujianrenn <wujianrenn@163.com>
2023-05-22 03:17:15 +0800

    first commit

---
+++ src/views/part5/components/MyList.vue
@@ -0,0 +1,50 @@
+<template>
+  <ul class="todo-main">
+        <MyItem 
+        v-for="todoObj in todos" 
+        :key="todoObj.id" 
+        :todo="todoObj" 
+        :checktodo="checktodo"
+        :shanchu="shanchu"
+        />      
+    </ul>
+</template>
+
+<script>
+import MyItem from './MyItem.vue';
+export default {
+name:'MyList',
+components: {MyItem},
+    
+    // data() {
+    //     return {
+    //         todos:[
+    //             {id:'001',title:'吃饭',done:true},
+    //             {id:'002',title:'喝酒',done:false},
+    //             {id:'003',title:'抽烟',done:true},
+    //         ]
+    //     }
+    // },
+    props:['todos','checktodo','shanchu']
+
+}
+</script>
+
+<style scoped>
+/* main */
+.todo-main{
+    margin-left: 0px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding: 0px;
+}
+
+.todo-empty{
+    height: 40px;
+    line-height: 40px;
+    border: 1px solid #ddd;
+    border-radius: 2px;
+    padding-left: 5px;
+    margin-top: 10px;
+}
+</style>
