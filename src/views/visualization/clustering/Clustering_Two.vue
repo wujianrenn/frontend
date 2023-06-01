e625cf7b11640cbded817f1311f2a4ad67d8184b
wujianrenn <wujianrenn@163.com>
2023-06-01 09:22:46 +0800

    first  commit

---
+++ src/views/visualization/clustering/Clustering_Two.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="dataing-container">
+       
+        <div class="middle box">
+            <img src="../../../../public/clustering_two/kmeans聚类1.png" alt="" style="width: 95%; height: 450px" >
+           <!-- <iframe src="../../../../public/clustering_png/lda聚类图.html" style="width: 570px;" height="450px"> -->
+            <div>
+                <img src="../../../../public/clustering_two/kmeans聚类2.png" alt="" style="width: 95%; height: 450px" >
+           </div>
+        </div>
+        <div class="right box">
+            <img src="../../../../public/clustering_two/kmeans聚类3.png" alt="" style="width: 95%; height: 450px" >
+            <div>
+                <!-- <iframe src="../../../../clustering_two/teo_1.html"   frameborder="0" style="width: 100%; height: 450px"  id="iframeBox" allowfullscreen  ></iframe> -->
+                <img src="../../../../public/clustering_two/two_1.png" alt="" style="width: 95%; height: 450px" >
+            </div>
+        </div>
+        
+    </div>
+  </template>
+
+  <script>
+  export default {
+      
+  };
+</script>
+  
+<style scoped>
+    .dataing-container {
+        display: flex;
+        
+    }
+
+    .middle {
+        display: inline-block;
+        
+        height: 100%;
+        width: 50%;
+        background-color:#fff;
+    }
+
+    .right {
+        display: inline-block;
+        height: 100%;
+        width: 50%;
+        /* background-color: greenyellow; */
+    }
+</style>
+  
